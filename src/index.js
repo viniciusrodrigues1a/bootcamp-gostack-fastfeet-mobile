ed05564b16358eddf7ef1ff69daf4f0a599eeab8
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-08 10:09:13 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Index() {
+  return <Text>Hello World</Text>;
+}

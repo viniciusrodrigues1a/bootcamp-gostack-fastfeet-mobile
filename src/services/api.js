0c6155fde57bf5ebc8f68bee6e8453ded3c1cc3c
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-14 19:56:54 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://10.0.3.2:3334', // Physical device
-  baseURL: 'http://localhost:3334', // Emulator
+  // baseURL: 'http://10.0.3.2:3334', // Emulator
+  baseURL: 'http://localhost:3334', // Physical device
 });
 
 export default api;
